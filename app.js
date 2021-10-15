c634c11238b068322943f7c293e3ea928e9befd1
KarenAmor <karedimor@gmail.com>
2021-10-15 15:34:18 -0500

    Se creo la carpeta public, views, package.json, nodemon y express. Sumado a lo anterior se vincularon los archivos HTML, CSS, JS. El Html tiene una estructura definida y con CSS se le agrego color

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3500;
 const publicFolderPath= path.resolve(__dirname, './public')
 app.use(express.static(publicFolderPath));
 app.get('/', (req, res)=> {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
 
 
