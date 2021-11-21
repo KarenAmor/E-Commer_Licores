220b7e4e48372019318a80070471f48a55d68200
Sr_santi <santiriosolaya@gmail.com>
2021-11-21 14:00:34 -0500

    refactor: change and connect style and functional structures

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3500;
-const publicFolderPath= path.resolve(__dirname, './public')
-app.use(express.static(publicFolderPath));
+// const publicFolderPath= path.resolve(__dirname, '/public')
+// app.use(express.static(publicFolderPath));
+app.use(express.static(path.join(__dirname, '/public')))
+
 app.get('/', (req, res)=> {
     res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
