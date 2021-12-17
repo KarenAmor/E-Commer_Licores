835f78fefb756d6836bd22def48396cc003edea3
fayala92 <92346063+fayala92@users.noreply.github.com>
2021-12-17 20:37:52 -0300

    se corrigió las rutas que estaban mal de imagenes y demás

--- public/js_files/compras.js
+++ public/js_files/compras.js
@@ -11,16 +11,16 @@ class Producto {
 }
 
 //Productos
-let p1 = new Producto("Whisky", "Jack Daniels", "Honey", "$4400 ARS", "/img/1.png")
-let p2 = new Producto("Whisky", "Johnny Walker", "Black Label", "$4000 ARS", "/img/2.png")
-let p3 = new Producto("Whisky", "Chivas Regal", "12y Blended Scotch", "$4500 ARS","/img/3.png")
-let p4 = new Producto("Vino", "Finca Altamira", "Malbec 2015", "$6000 ARS", "/img/4.png")
-let p5 = new Producto("Vino", "Los Haroldos", "Chardonnay 2013", "$5000 ARS", "/img/5.png")
-let p6 = new Producto("Vino", "Cavit", "Cabernet Sauvignon", "$1700 ARS", "/img/6.png")
-let p7 = new Producto("Vino", "Henri Lurton", "Sauvignon", "$3200 ARS", "/img/7.png")
-let p8 = new Producto("Cerveza", "Andes Origen 1L", "Roja", "$170 ARS", "/img/8.png")
-let p9 = new Producto("Cerveza", "Corona 330ml", "Rubia", "$150 ARS", "/img/9.png")
-let p10 = new Producto("Cerveza", "Club Colombia 330ml", "Negra", "$670 ARS", "/img/10.png")
+let p1 = new Producto("Whisky", "Jack Daniels", "Honey", "$4400 ARS", "/public/img/1.png")
+let p2 = new Producto("Whisky", "Johnny Walker", "Black Label", "$4000 ARS", "/public/img/2.png")
+let p3 = new Producto("Whisky", "Chivas Regal", "12y Blended Scotch", "$4500 ARS","/public/img/3.png")
+let p4 = new Producto("Vino", "Finca Altamira", "Malbec 2015", "$6000 ARS", "/public/img/4.png")
+let p5 = new Producto("Vino", "Los Haroldos", "Chardonnay 2013", "$5000 ARS", "/public/img/5.png")
+let p6 = new Producto("Vino", "Cavit", "Cabernet Sauvignon", "$1700 ARS", "/public/img/6.png")
+let p7 = new Producto("Vino", "Henri Lurton", "Sauvignon", "$3200 ARS", "/public/img/7.png")
+let p8 = new Producto("Cerveza", "Andes Origen 1L", "Roja", "$170 ARS", "/public/img/8.png")
+let p9 = new Producto("Cerveza", "Corona 330ml", "Rubia", "$150 ARS", "/public/img/9.png")
+let p10 = new Producto("Cerveza", "Club Colombia 330ml", "Negra", "$670 ARS", "/public/img/10.png")
 
 //Lista de productos
 let productos = [p1,p2,p3,p4,p5,p6,p7,p8,p9,p10]
